20123c5daa5b6718cf3d56cc1113f7f465b61767
Lucas Cupertino <olucas.cdev@gmail.com>
2023-05-03 16:55:27 -0300

    adição arquivos

---
+++ BankTest1/BankAccountTests.cs
@@ -0,0 +1,46 @@
+using BankAccountNS;
+
+namespace BankTest1
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            try
+            {
+                account.Credit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                //Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountLessThanZeroMessage);
+            }
+        }
+
+    }
+
+}
